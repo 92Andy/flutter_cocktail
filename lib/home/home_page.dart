2210c6a145eec06b29b7793465c4564e859ad9c4
Andy Eckhardt <eckhardtandy@gmail.com>
2022-05-01 20:19:36 +0200

    random cocktail widgets added (WIP)

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cocktail/home/cubit/home_cubit.dart';
@@ -48,11 +49,74 @@ class _HomeView extends StatelessWidget {
             children: const [
               HomeTitle(),
               SizedBox(height: 50),
-              SearchBar(),
+              SearchBar(), //Todo: search callback should be added
+              SizedBox(height: 50),
+              RandomCocktail(), //Todo: get and add random cocktail + refresh callback
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class RandomCocktail extends StatelessWidget {
+  const RandomCocktail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              'Random cocktail',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              constraints: BoxConstraints(),
+              onPressed: null, //Todo: refresh cocktail
+              icon: Icon(Icons.refresh),
+            )
+          ],
+        ),
+        const SizedBox(height: 20),
+        const BigCocktailCard(),
+      ],
+    );
+  }
+}
+
+class BigCocktailCard extends StatelessWidget {
+  const BigCocktailCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: CachedNetworkImage(
+        imageUrl:
+            'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.fill,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        placeholder: (context, url) =>
+            const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+            const Center(child: Icon(Icons.error)),
+      ),
     );
   }
 }
