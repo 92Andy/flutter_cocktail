8becc7e9df52e74a5123dd19e0f30f04593a3550
Andy Eckhardt <eckhardtandy@gmail.com>
2022-05-05 22:59:42 +0200

    multi-language-support: default language file added

---
+++ lib/languages/language_en.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_cocktail/languages/languages.dart';
+
+class LanguageEn extends Languages {
+  // HomePage
+  @override
+  String get homePageBoldPartTitle => throw UnimplementedError();
+
+  @override
+  String get homePageNormalPartTitle => throw UnimplementedError();
+
+  @override
+  String get randomCocktailTitle => throw UnimplementedError();
+
+  // SearchPage
+  @override
+  String get searchPageBoldPartTitle => throw UnimplementedError();
+
+  @override
+  String get searchPageNormalPartTitle => throw UnimplementedError();
+
+  // FavoritesPage
+  @override
+  String get favoritesPageBoldPartTitle => throw UnimplementedError();
+
+  @override
+  String get favoritesPageNormalPartTitle => throw UnimplementedError();
+
+  // SettingsPage
+  @override
+  String get settingsPageBoldPartTitle => throw UnimplementedError();
+
+  @override
+  String get settingsPageNormalPartTitle => throw UnimplementedError();
+}
