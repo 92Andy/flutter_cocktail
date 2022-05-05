9f9409c3733c938f6cbacb133059464396e91299
Andy Eckhardt <eckhardtandy@gmail.com>
2022-05-05 23:03:28 +0200

    main page outsourced

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cocktail/favorites/favorites_page.dart';
-import 'package:flutter_cocktail/search/search_page.dart';
-import 'package:flutter_cocktail/settings/settings_page.dart';
-
-import 'widgets/bottom_nav_bar.dart';
-import 'home/home_page.dart';
-import 'widgets/background_image.dart';
+import 'package:flutter_cocktail/main_page.dart';
 
 void main() {
   runApp(
@@ -22,57 +16,4 @@ void main() {
       ),
     ),
   );
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int currentPageIndex = 0;
-  final listOfPages = <Widget>[
-    const HomePage(),
-    const SearchPage(),
-    const FavoritesPage(),
-    const SettingsPage(),
-  ];
-  late PageController pageController;
-
-  @override
-  void initState() {
-    pageController = PageController(initialPage: 0);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      backgroundColor: Colors.transparent,
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: currentPageIndex,
-        iconTapped: (int index) => setState(() {
-          currentPageIndex = index;
-          pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeIn,
-          );
-        }),
-      ),
-      body: Stack(
-        children: [
-          const BackgroundImage(),
-          PageView(
-            physics: const NeverScrollableScrollPhysics(),
-            controller: pageController,
-            children: listOfPages,
-          ),
-        ],
-      ),
-    );
-  }
 }
